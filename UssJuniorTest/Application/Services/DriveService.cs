0e903140d8e76e655faad3e6172b06cfc9e12aed
flashikNet <133534020+flashikNet@users.noreply.github.com>
2024-04-16 01:14:32 +0500

    fix difficult moment
    now should work with different ranges

--- UssJuniorTest/Application/Services/DriveService.cs
+++ UssJuniorTest/Application/Services/DriveService.cs
@@ -20,33 +20,7 @@ namespace UssJuniorTest.Application.Services
         }
         public GetDrivesRes[] GetDrives(GetDrivesReq req)
         {
-            //Console.WriteLine(driveLogRepository.GetAll().First().EndDateTime - driveLogRepository.GetAll().First().StartDateTime);
-            //var res = driveLogRepository.GetAll().Join(carRepository.GetAll(),
-            //    d => d.CarId, c => c.Id,
-            //    (d, c) => new
-            //    {
-            //        PersonId = d.PersonId,
-            //        StartDateTime = d.StartDateTime,
-            //        EndDateTime = d.EndDateTime,
-            //        Manufacturer = c.Manufacturer,
-            //        Model = c.Model,
-            //    })
-            //    .Join(personRepository.GetAll(),
-            //    d => d.PersonId,
-            //    p => p.Id,
-            //    (d, person) => new GetDrivesRes
-            //    {
-            //        Name = person.Name,
-            //        Age = person.Age,
-            //        Manufacturer = d.Manufacturer,
-            //        Model = d.Model,
-            //        Days = (Min(req.End, d.EndDateTime) - Max(req.Start, d.StartDateTime)).Days,
-            //        Hours = (Min(req.End, d.EndDateTime) - Max(req.Start, d.StartDateTime)).Hours,
-            //        Minutes = (Min(req.End, d.EndDateTime) - Max(req.Start, d.StartDateTime)).Minutes
-
-            //    });
             var res = from driveLog in driveLogRepository.GetAll()
-                      where driveLog.StartDateTime.CompareTo(req.Start) >= 0 || driveLog.EndDateTime.CompareTo(req.End) <= 0
                       join person in personRepository.GetAll() on driveLog.PersonId equals person.Id
                       join car in carRepository.GetAll() on driveLog.CarId equals car.Id
                       select new GetDrivesRes
@@ -59,18 +33,24 @@ namespace UssJuniorTest.Application.Services
                           Hours = (Min(req.End, driveLog.EndDateTime) - Max(req.Start, driveLog.StartDateTime)).Hours,
                           Minutes = (Min(req.End, driveLog.EndDateTime) - Max(req.Start, driveLog.StartDateTime)).Minutes
                       };
-            return res.ToArray();
+            return res.Where(x => x.Days >=0 && x.Hours >= 0 && x.Minutes >= 0).ToArray();
         }
 
         private DateTime Max(DateTime dt1, DateTime dt2)
         {
-            if(dt1.CompareTo(dt2) > 0) return dt1;
+            if (dt1.CompareTo(dt2) > 0)
+            {
+                return dt1;
+            }
             return dt2;
         }
 
         private DateTime Min(DateTime dt1, DateTime dt2)
         {
-            if (dt1.CompareTo(dt2)<0) return dt1;
+            if (dt1.CompareTo(dt2) < 0)
+            {
+                return dt1;
+            }
             return dt2;
         }
     }
